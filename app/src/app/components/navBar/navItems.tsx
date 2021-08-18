6544014c4d35dadf076d2d0a32f60ae940f66e68
amolTelang <telangamol0909@gmail.com>
2021-08-18 20:30:46 +0530

    added hamburger Menu

--- app/src/app/components/navBar/navItems.tsx
+++ app/src/app/components/navBar/navItems.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import styled from "styled-components";
+import styled,{css} from "styled-components";
 import tw from "twin.macro";
-
-
-
+import { slide as Menu } from 'react-burger-menu';
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../resposive";
+import menuStyles from "./menuStyles";
 const ListContainer=styled.ul`
 ${tw`
 flex
 list-none
 `};`;
 
-const NavItem=styled.li`
+const NavItem=styled.li<{menu?: any}>`
 ${tw`
-text-xs
+text-sm
 md:text-base
 text-black
 font-medium
@@ -24,10 +25,42 @@ duration-300
 ease-in-out
 hover:text-gray-700
 
-`};`;
+`};
+${({ menu }) =>
+menu &&
+css`
+  ${tw`
+  text-white
+  text-xl
+  mb-3
+  focus:text-white
+`};
+`};
+`;
 
 
 export function NavItems(){
+
+    const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+    if (isMobile)
+    return (
+      <Menu right styles={menuStyles}>
+        <ListContainer>
+          <NavItem menu>
+            <a href="#">Home</a>
+          </NavItem>
+          <NavItem  menu >
+            <a href="#">Cars</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Contact Us</a>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
 return <ListContainer>
     <NavItem>
         <a href="#">Home</a>
